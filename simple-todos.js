f84cd542595274619e59895fa7e2c93b53a6a12f
Steve Witman <stevewitman@gmail.com>
2015-05-12 23:10:01 -0600

    adding tasks with a form

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,19 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      // console.log(event);
+      event.target.text.value = "";
+      return false;
     }
   });
 }

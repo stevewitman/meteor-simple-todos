1d213cc5eb1c0e22971807c8d53a2406217c9246
Steve Witman <stevewitman@gmail.com>
2015-05-13 11:32:14 -0600

    storing temporary UI state in session

--- simple-todos.js
+++ simple-todos.js
@@ -3,7 +3,19 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+      hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+      incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
   Template.body.events({
@@ -16,6 +28,9 @@ if (Meteor.isClient) {
       // console.log(event);
       event.target.text.value = "";
       return false;
+    },
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
   Template.task.events({
